7a89db2ea5c22c354d9b0fa8216e5742ee7d5c9b
John Koster <kosterjo@umich.edu>
2016-07-20 11:03:35 -0700

    made web page frameworks

--- mysite/leasingPortal/models.py
+++ mysite/leasingPortal/models.py
@@ -3,9 +3,11 @@ from __future__ import unicode_literals
 from django.db import models
 from django import forms
 import django_extensions
+from django_extensions.db.fields import ModificationDateTimeField
 
 class Building(models.Model):
-	address = models.CharField(max_length=200)
+	address  = models.CharField(max_length=200)
+	modified = ModificationDateTimeField()
 
 	def __str__(self):
 		return self.address
@@ -28,6 +30,7 @@ class Suite(models.Model):
 	min_lease_term  = models.IntegerField(null = True, blank = True)
 	rental_rate     = models.DecimalField(max_digits = 3, decimal_places = 2)
 	size            = models.IntegerField()
+	modified        = ModificationDateTimeField()
 
 	def __str__(self):
 		return "Suite#" + str(self.number)
