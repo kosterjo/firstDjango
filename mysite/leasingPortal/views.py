54e82b2803d425e8fd6e2e771483f89063de8ccd
John Koster <kosterjo@umich.edu>
2016-07-25 15:57:34 -0700

    updated .gitignore and displayed building information in index.html

--- mysite/leasingPortal/views.py
+++ mysite/leasingPortal/views.py
@@ -2,10 +2,16 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
+from .models import Building
+
 def index(request):
+	building_list = Building.objects.order_by('address')
 	template = loader.select_template(['leasingPortal/index.html',
 		                                'leasingPortal/base.html'])
-	return HttpResponse(template.render(request))
+	context = {
+	  'building_list': building_list,
+	}
+	return HttpResponse(template.render(context, request))
 
 def buildings(request, number):
 	return HttpResponse('Hey you chose %s.' % number)
