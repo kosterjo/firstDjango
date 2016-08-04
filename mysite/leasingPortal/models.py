10fb1bff1f53c49ff74909026ff64794fde2f034
John Koster <kosterjo@umich.edu>
2016-08-04 16:08:39 -0700

    ignored migrations and simplified the suite model -- removed complicated properties

--- mysite/leasingPortal/models.py
+++ mysite/leasingPortal/models.py
@@ -13,23 +13,9 @@ class Building(models.Model):
 		return self.address
 
 class Suite(models.Model):
-	pay_choice = (
-  	('YR', 'yearly'), 
-  	('MO', 'monthly'),
-	)
-
 	parent_building = models.ForeignKey(Building, on_delete=models.CASCADE)
 	number          = models.IntegerField()
-	notes           = models.TextField(null = True, blank = True)
 	date_available  = models.DateTimeField(null = True)
-	access          = models.TextField()
-	#annually        = forms.ChoiceField(choices=pay_choice)
-	available       = models.BooleanField()
-	extra_net_costs = models.DecimalField(null = True, blank = True, max_digits = 3, decimal_places = 2)
-	#payment_type    = forms.ChoiceField(choices=['MG', 'FSG', 'NNN'])
-	min_lease_term  = models.IntegerField(null = True, blank = True)
-	rental_rate     = models.DecimalField(max_digits = 3, decimal_places = 2)
-	size            = models.IntegerField()
 	modified        = ModificationDateTimeField()
 
 	def __str__(self):
