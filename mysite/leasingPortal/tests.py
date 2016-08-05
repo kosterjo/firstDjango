2ca8e4a709f7358db09ccbf33d1e3b45546c4281
John Koster <kosterjo@umich.edu>
2016-08-05 11:42:47 -0700

    added create_suite function to tests

--- mysite/leasingPortal/tests.py
+++ mysite/leasingPortal/tests.py
@@ -4,9 +4,13 @@ from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
-from .models import Building
+from .models import Building, Suite
 
 def create_building(address):
 	return Building.objects.create(address=address)
 
+def create_suite(parent, number, available):
+	date = datetime.datetime(available[0], available[1], available[2]).date()
+	return Suite.objects.create(parent_building=parent, number=number, 
+		     date_available=date)
 
