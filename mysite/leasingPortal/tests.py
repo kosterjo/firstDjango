69c8c25d64878099666d586d1065b5d518406975
John Koster <kosterjo@umich.edu>
2016-08-05 13:45:47 -0700

    added helpful message when a building has no suites in building_details page

--- mysite/leasingPortal/tests.py
+++ mysite/leasingPortal/tests.py
@@ -27,10 +27,16 @@ class BuildingViewTests(TestCase):
 
 	def test_buildings_view_with_buildings(self):
 		'''
-		if buildigns exist, no message should be displayed
+		if buildings exist, no message should be displayed
 		'''
 		create_building("new_building")
 		response = self.client.get(reverse('leasingPortal:buildings'))
 		self.assertEqual(response.status_code, 200)
 		self.assertNotContains(response, "you haven't uploaded a building yet!")
 		self.assertQuerysetEqual(response.context['building_list'], ['<Building: new_building>'])
+
+	def test_building_details_view_without_suites(self):
+		'''
+		if building has no children suites, an appropriate message 
+		should be displayed
+		'''
