0b14dbaa7ae002634eeaa1ed32bf69836f637235
John Koster <kosterjo@umich.edu>
2016-08-05 11:29:02 -0700

    added create building function to tests

--- mysite/leasingPortal/tests.py
+++ mysite/leasingPortal/tests.py
@@ -1,3 +1,12 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.core.urlresolvers import reverse 
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Building
+
+def create_building(address):
+	return Building.objects.create(address=address)
+
+
